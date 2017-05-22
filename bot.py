844c0e20d69ba06025d5c727a241821fb864c1e1
Dion Sorel <glaze.10@outlook.com>
2017-05-22 16:14:48 +0200

    More updates

--- bot.py
+++ bot.py
@@ -29,6 +29,8 @@ else:
 
 initial_extensions = [
     'cogs.moderation',
+    'cogs.botadmin',
+    
 
 ]
 
@@ -210,6 +212,7 @@ class Music:
             player.resume()
 
     @commands.command(pass_context=True, no_pm=True)
+    @checks.is_owner()
     async def stop(self, ctx):
         """Stops playing audio and leaves the voice channel.
         This also clears the queue.
---
+++ cogs/botadmin.py
@@ -0,0 +1,62 @@
+from discord.ext import commands
+from .utils import checks
+import discord
+import inspect
+import asyncio
+
+
+import datetime
+from collections import Counter
+
+
+class Botadmin:
+	"""Admin only commands"""
+
+
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	@commands.command(hidden=True)
+	@checks.is_owner()
+	async def load(self, *, module : str):
+		"""Loads a module"""
+		try:
+			self.bot.load_extension(module)
+		except Exception as e:
+			await self.bot.say('\N{PISTOL}')
+			await self.bot.say('{}: {}'.format(type(e).__name__, e))
+		else:
+			await self.bot.say('\N{OK HAND SIGN}')
+
+
+	@commands.command(Hidden=True)
+	@checks.is_owner()
+	async def unload(self, *, module : str):
+		"""Unloads a module"""
+		try:
+			print('Unloading {}'.format(module))
+			self.bot.unload_extension(module)
+		except Exception as e:
+			await self.bot.say('\N{PISTOL}')
+			await self.bot.say('{}: {}'.format(type(e).__name__, e))
+		else:
+			await self.bot.say('\N{OK HAND SIGN}')
+
+
+	@commands.command(name='reload', hidden=True)
+	@checks.is_owner()
+	async def _reload(self, *, module : str):
+		"""Reloads a module"""
+		try:
+			self.bot.unload_extension(module)
+			self.bot.load_extension(module)
+		except Exception as e:
+			await self.bot.say('\N{PISTOL}')
+			await self.bot.say('{}: {}'.format(type(e).__name__, e))
+		else:
+			await self.bot.say('\N{OK HAND SIGN}')
+
+
+def setup(bot):
+	bot.add_cog(Botadmin(bot))
