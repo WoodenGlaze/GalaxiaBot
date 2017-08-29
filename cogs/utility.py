a5ed70c5945fda0e3ec3c8d07cd34bdedf5bca68
Tony Stark <glaze.10@outlook.com>
2017-08-29 08:37:47 -0700

    Reroll

--- cogs/utility.py
+++ cogs/utility.py
@@ -147,9 +147,11 @@ class Utility:
 					status = 'Behind by 1 release%s [Latest update.](%s)' % (branch_note, gist_latest)
 				else:
 					status = '%s releases behind%s [Latest updates.](%s)' % (version, branch_note, gist_latest)
+				except exception as e:
+					pass
 
 
-			await self.bot.say(embed=em)
+				await self.bot.say(embed=em)
 
 
 
