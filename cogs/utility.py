6375fe4eb146ab18185ea3b17ccf12f8affb57f4
Tony Stark <glaze.10@outlook.com>
2017-08-29 08:40:42 -0700

    Final fix

--- cogs/utility.py
+++ cogs/utility.py
@@ -147,8 +147,7 @@ class Utility:
 					status = 'Behind by 1 release%s [Latest update.](%s)' % (branch_note, gist_latest)
 				else:
 					status = '%s releases behind%s [Latest updates.](%s)' % (version, branch_note, gist_latest)
-				if True == True:
-					pass
+
 
 
 		await self.bot.say(embed=em)
